a2a6bd2ce4dd628d43eb796b8afd1f8e5ea195bb
Khushi S <khushi8.shaha@gmail.com>
2025-04-12 11:43:32 -0400

    Initial commit with working frontend

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// Script to handle file upload interaction
+document.getElementById('uploadButton').addEventListener('click', function() {
+    var fileInput = document.getElementById('fileInput');
+    var file = fileInput.files[0];
+
+    if (file) {
+        alert('File selected: ' + file.name);
+    } else {
+        alert('Please select a file first');
+    }
+});
 
